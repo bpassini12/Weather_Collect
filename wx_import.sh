b6377ce1198475ac582e35a281f38ab55ab15d78
Brett Passini <Brett.Passini@Gmail.com>
2023-11-07 20:34:02 -0600

    adding shell script

---
+++ wx_import.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+#cd /home/bpassini/code/PROJECTS/Ercot_LMP_Scrape/
+source /home/bpassini/envs/venv_bp/bin/activate
+python /home/bpassini/code/PROJECTS/Weather_Collect/import_wx_data.py
